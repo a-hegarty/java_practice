fdba65d1b74213c07b92087515727408730ae5c1
A.Hegarty <a.hegarty004@gmail.com>
2025-02-26 14:41:21 +0000

    encapsulation example complete

--- Example.java
+++ Example.java
@@ -1,20 +1,70 @@
+import java.util.ArrayList;
+
+//main class
 public class Example {
-    //attributes
-    private String name;
-    private int amount;
+    public static void main(String[] args) {
+        Inventory inventory = new Inventory();
 
-    //constructor
-    public Item(String name, int amount) {
-        //this.___ lets params be the same as private attributes above
-        this.name = name;
-        this.amount = amount;
+        //initialise items
+        Item item_1 = new Item("apples", 20);
+        Item item_2 = new Item("sword", 1);
+
+        //add items to inventory
+        inventory.addItem(item_1);
+        inventory.addItem(item_2);
+
+        //print items in inventory
+        inventory.display_Inventory();
+    }
+}
+
+class Item {
+     //attributes
+     private String name;
+     private int amount;
+ 
+     //constructor
+     //constructors must be named the same as a class
+     public Item(String name, int amount) {
+         //this.___ lets params be the same as private attributes above
+         this.name = name;
+         this.amount = amount;
+     }
+ 
+     public String get_Name() {
+         return name;
+     }
+ 
+     public int get_Amount() {
+         return amount;
+     }
+}
+
+class Inventory{
+    //ArrayList stores objects
+    //differetn to arrays, which cannot store objects, only datatypes
+
+    //declaring ArrayList 'items', which stores objects from class Example
+    private ArrayList<Item> items;
+
+    //constructor for class inventory
+    //assigning value of new, empty arraylist to arraylist declared above
+    public Inventory() {
+        items = new ArrayList<>();
     }
 
-    public String get_Name() {
-        return name;
+    //method adds item to arraylist 'item' created above
+    public void addItem(Item item) {
+        items.add(item);
     }
 
-    public int get_Amount() {
-        return amount;
+    public void display_Inventory() {
+        for(Item item : items) {
+            System.out.println("Item: " + item.get_Name() + ", Amount: " + item.get_Amount());
+        }
     }
+}
+
+class Fruit {
+    private 
 }
--- Main.java
+++ Main.java
@@ -3,15 +3,20 @@ public class Main {
         String greeting = "hello";
         
         int greet_Length = greeting.length();
-        int number = 10;
+        //int number = 10;
 
-        int total = math_Utils.add(greet_Length, number);
+        //int total = math_Utils.add(greet_Length, number);
 
-        int likes = 150;
-        int dislikes = 50;
+        //int likes = 150;
+        //int dislikes = 50;
 
-        double ratio = math_Utils.ratio(likes, dislikes);
+        //double ratio = math_Utils.ratio(likes, dislikes);
 
-        System.out.println(ratio);
+        //System.out.println(ratio);
+
+        //for loop prints char at position i
+        for(int i = 0; i < greet_Length; i ++) {
+            System.out.println(greeting.charAt(i));
+        }
     }
 }
